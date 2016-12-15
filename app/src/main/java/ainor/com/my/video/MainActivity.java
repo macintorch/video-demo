301dfc7643641b3c28ef49889a046a05cd1c652a
Ainor Syahrizal <macintorch@gmail.com>
2016-12-15 21:22:31 +0800

    add media controller

--- app/src/main/java/ainor/com/my/video/MainActivity.java
+++ app/src/main/java/ainor/com/my/video/MainActivity.java
@@ -3,6 +3,7 @@ package ainor.com.my.video;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,13 @@ public class MainActivity extends AppCompatActivity {
         VideoView videoView = (VideoView) findViewById(R.id.videoView);
 
         videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.entrepreneur);
+
+        MediaController mediaController = new MediaController(this);
+
+        mediaController.setAnchorView(videoView);
+
+        videoView.setMediaController(mediaController);
+
         videoView.start();
     }
 }
